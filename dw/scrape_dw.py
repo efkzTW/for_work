42287cc7b33515cdf4e73b4de2e5519e167b5626
unknown <Peter.Wu@CNU9425BDG.dishhdasia.com>
2013-07-31 15:01:57 +0800

    begin new script

---
+++ dw/scrape_dw.py
@@ -0,0 +1,10 @@
+import mechanize
+from urllib2 import urlopen
+
+url = "http://www.dw.de/program/tv-program-guide/s-4757-9801"
+br = mechanize.Browser()
+
+
+
+if __name__ == '__main__':
+	print "let's begin"
